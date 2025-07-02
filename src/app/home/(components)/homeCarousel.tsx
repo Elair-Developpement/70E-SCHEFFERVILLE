13b36e6b60d7b2b0bdbd80c600f08380938d3c41
Louis Robitaille Dionne <78739122+LouisRDionne@users.noreply.github.com>
2025-07-02 15:08:40 -0400

    Supprimés pages et images boilerplate, ajouté images de contact et partenaires, Carousel avance

---
+++ src/app/home/(components)/homeCarousel.tsx
@@ -0,0 +1,38 @@
+import Image from "next/image";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+
+export default function HomeCarousel() {
+  return (
+    <Carousel
+      className="w-full h-full"
+      opts={{
+        loop: true,
+      }}
+    >
+      <CarouselContent>
+        {["aurore", "aurore2", "barrage"].map((img, i) => (
+          <CarouselItem key={i} className="flex items-center justify-center">
+            <div className="relative w-300 h-200 overflow-hidden rounded-lg">
+              <Image
+                src={`/images/carousel/${img}.jpg`}
+                alt={img}
+                fill
+                className="object-cover"
+                sizes="100vw"
+                priority={i === 0}
+              />
+            </div>
+          </CarouselItem>
+        ))}
+      </CarouselContent>
+      <CarouselPrevious />
+      <CarouselNext />
+    </Carousel>
+  );
+}
--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,9 +1,11 @@
 import Header from "@/components/running/header";
+import HomeCarousel from "./(components)/homeCarousel";
 
 export default function Home() {
   return (
-    <main className="pt-24">
+    <main className="container mx-auto pt-80">
       <Header />
+      <HomeCarousel />
       <p>Hello World !</p>
       <h1>Bienvenue sur le site de la 70e Schefferville</h1>
     </main>
