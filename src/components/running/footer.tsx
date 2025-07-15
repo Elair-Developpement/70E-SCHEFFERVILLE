33781e3578233bc2695fdb185a4e049065b2ad02
Louis Robitaille Dionne <78739122+LouisRDionne@users.noreply.github.com>
2025-07-15 15:08:40 -0400

    Utilisé variable t dans footer afion de permettre le build

--- src/components/running/footer.tsx
+++ src/components/running/footer.tsx
@@ -6,7 +6,7 @@ export default function Footer() {
   return (
     <footer className="flex w-full rounded-t-2xl mx-auto bg-blue-1 text-white py-4 mt-4 md:max-w-[80rem]">
       <div className="container mx-auto text-center">
-        <p className="text-sm">Allo</p>
+        <p className="text-sm">{t("links")}</p>
       </div>
     </footer>
   );
