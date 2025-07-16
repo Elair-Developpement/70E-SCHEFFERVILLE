a96f357e56a0eabb52a7640abd5767cff5414028
Louis Robitaille Dionne <78739122+LouisRDionne@users.noreply.github.com>
2025-07-16 10:56:17 -0400

    Ajouté interface pour communiqués

---
+++ src/lib/types/press.ts
@@ -0,0 +1,10 @@
+export default interface Press {
+  title: string,
+  title_eng: string,
+  body: string,
+  body_eng: string,
+  footer: string,
+  footer_eng: string,
+  image_link: string,
+  link: string
+}
