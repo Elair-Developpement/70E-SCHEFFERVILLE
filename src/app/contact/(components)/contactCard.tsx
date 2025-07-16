18047b735e966fbc4f697c4602d5773d3468ea7f
Louis Robitaille Dionne <78739122+LouisRDionne@users.noreply.github.com>
2025-07-16 16:09:00 -0400

    Changé nom de photo dans nous joindre et ajusté taille de police

--- src/app/contact/(components)/contactCard.tsx
+++ src/app/contact/(components)/contactCard.tsx
@@ -23,12 +23,12 @@ export default function ContactCard({
     <div className="flex">
       <Image
         src={`/images/contact/${photo}`}
-        alt="Julie Munger"
+        alt={name}
         width={666}
         height={1000}
         className="w-2/5 rounded-2xl md:max-w-1/5"
       />
-      <div className="flex flex-col justify-center ps-2 w-3/5 gap-1 md:gap-2">
+      <div className="flex flex-col justify-center ps-2 md:ps-6 w-3/5 gap-1 md:gap-2">
         <h3 className="text-2xl md:text-4xl font-bold text-blue-1">{name}</h3>
         <p className="text-lg md:text-2xl text-blue-2">{title}</p>
         <div className="max-md:flex-2 md:flex-row md:flex-nowrap wrap-break-word text-lg md:text-2xl">
--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -9,10 +9,10 @@ export default function Contact() {
   return (
     <main className="block pt-[5rem] px-2 md:max-w-[80rem] md:mx-auto min-h-[calc(100vh-25rem)]">
       <BackToHomeButton />
-      <h1 className="text-3xl md:text-5xl text-center font-bold text-blue-1">
+      <h1 className="text-3xl md:text-3xl text-center font-bold text-blue-1">
         {t("title")}
       </h1>
-      <h2 className="text-xl md:text-3xl text-center font-bold text-blue-2">
+      <h2 className="text-xl md:text-xl text-center font-bold text-blue-2">
         {t("people")}
       </h2>
       <div className="flex flex-col mt-2">
@@ -25,7 +25,7 @@ export default function Contact() {
             phone="418-925-7475"
           />
           <ContactCard
-            photo="Ghislain Lévesque 2025.jpg"
+            photo="Ghislain Levesque 2025.JPG"
             name="Ghislain Lévesque"
             title={t("ghislain-title")}
             email="glevesque@cgl25.ca"
