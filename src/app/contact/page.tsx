308aab5688d0737425786fa3a3447e05f8847788
Louis Robitaille Dionne <78739122+LouisRDionne@users.noreply.github.com>
2025-07-17 09:23:26 -0400

    Ajusté la page de contacts

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -21,15 +21,15 @@ export default function Contact() {
             photo="Julie Munger 2025.jpg"
             name="Julie Munger"
             title={t("julie-title")}
-            email="communautaire@schefferville.ca"
-            phone="418-925-7475"
+            email="developpement@schefferville.ca"
+            phone="418 925-7475"
           />
           <ContactCard
             photo="Ghislain Levesque 2025.JPG"
             name="Ghislain Lévesque"
             title={t("ghislain-title")}
             email="glevesque@cgl25.ca"
-            phone="418-964-6275"
+            phone="418 964-6275"
             portrait_link="https://tplhwzejgzgrpqclrkse.supabase.co/storage/v1/object/public/documents/portrait/Portrait%20Ghislain%20Levesque.pdf"
           />
         </div>
